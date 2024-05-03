c245112511d691a20c205ae542a600139be7be18
Mo Kiani <mo.kiani.engineer@gmail.com>
2024-05-03 03:08:37 -0700

    change working directory of setup script to repo root

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Always run this script with its physical path as its working directory
-cd -P "$(dirname $(realpath "$0"))"
+# Always run this script with the repo's physical path as its working directory
+cd -P "$(dirname $(realpath "$0"))/.."
 echo "Running from absolute physical path: $(pwd)"

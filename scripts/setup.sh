cc293e7e5b29fcfa76fad704688528cd0bbe26f2
Mo Kiani <mo.kiani.engineer@gmail.com>
2024-05-03 03:30:21 -0700

    require sudo/root permissions for setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Require root/sudo permissions
+if [ "$EUID" -ne 0 ]; then
+    echo "This script requires root/sudo permissions. Try: sudo !!"
+    exit 1
+fi
+
 # Always run this script with the repo's physical path as its working directory
 cd -P "$(dirname $(realpath "$0"))/.."
 echo "Running from absolute physical path: $(pwd)"

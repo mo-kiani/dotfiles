dbad85fa43a3430b3abf42793cc44259545ce162
Mo Kiani <mo.kiani.engineer@gmail.com>
2024-05-09 03:38:53 -0700

    update mysetup.sh to work right with change to deployment via symlinks

--- scripts/mysetup.sh
+++ scripts/mysetup.sh
@@ -21,20 +21,20 @@ echo
 install_package unicode || true
 
 echo
-deploy_file dotfiles/inputrc ~/.inputrc
+deploy_file "$REPO_PATH/dotfiles/inputrc" ~/.inputrc
 echo
 move_over_file ~/.bashrc ~/.bashrc_default
-deploy_file dotfiles/bashrc ~/.bashrc
+deploy_file "$REPO_PATH/dotfiles/bashrc" ~/.bashrc
 echo
-deploy_file dotfiles/ssh/config ~/.ssh/config
+deploy_file "$REPO_PATH/dotfiles/ssh/config" ~/.ssh/config
 echo
-deploy_file dotfiles/tmux.conf ~/.tmux.conf
+deploy_file "$REPO_PATH/dotfiles/tmux.conf" ~/.tmux.conf
 echo
-deploy_file dotfiles/vimrc ~/.vimrc
+deploy_file "$REPO_PATH/dotfiles/vimrc" ~/.vimrc
 echo
-deploy_file dotfiles/gitconfig ~/.gitconfig
+deploy_file "$REPO_PATH/dotfiles/gitconfig" ~/.gitconfig
 echo
-deploy_file dotfiles/oh-my-posh/themes/mo.omp.json ~/.oh-my-posh/themes/custom/mo.omp.json
+deploy_file "$REPO_PATH/dotfiles/oh-my-posh/themes" ~/.oh-my-posh/themes/custom
 
 echo
 echo "Creating folder ~/.path, which bashrc will append to \$PATH"
